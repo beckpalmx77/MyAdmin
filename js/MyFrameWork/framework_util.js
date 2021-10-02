f5a906b4db573af31855f933fd437bef0a17e403
beckpalmx77 <beckpalmx@gmail.com>
2021-10-02 11:39:26 +0700

    Update 02-10-2021 11.39

--- js/MyFrameWork/framework_util.js
+++ js/MyFrameWork/framework_util.js
@@ -3,13 +3,13 @@ function isEmpty(obj) {
         return true;
     }
     return false;
-}
 
-/* How to Use
-let object = {}
-object.name = '';
-console.log(isEmpty(object));
-*/
+    /* How to Use
+    let object = {}
+    object.name = '';
+    console.log(isEmpty(object));
+    */
+}
 
 
 function ValidateEmail(inputText) {
